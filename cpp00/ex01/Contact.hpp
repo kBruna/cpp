4b0d6246b9492b262ef24b022ad5a26208a5767b
buehara <buehara@student.42sp.org.br>
2026-07-28 22:38:25 -0300

    chore: add PhoneBook and Contacts files

---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,18 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class	Contact
+{
+	private:
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickname;
+		std::string _phoneNumber;
+		std::string _darkestSecret;
+
+	public:
+}
+
+#endif
---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,14 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+chass PhoneBook
+{
+	private:
+		Contact _contact[8];
+
+	public:
+};
+
+#endif
